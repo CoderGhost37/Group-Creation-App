018aaaef7aa2f864c4b4938884898c3d1906e6a9
CoderGhost37 <coderghost37@gmail.com>
2025-05-17 14:23:16 +0530

    feat: ui components setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 
+import AuthWrapper from '@/components/providers/auth-provider'
+import { Toaster } from '@/components/ui/sonner'
+import { TooltipProvider } from '@/components/ui/tooltip'
+
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
@@ -24,7 +28,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <AuthWrapper>
+          <Toaster richColors />
+          <TooltipProvider>{children}</TooltipProvider>
+        </AuthWrapper>
+      </body>
     </html>
   )
 }
